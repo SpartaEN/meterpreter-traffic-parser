9f1fff552085af440ce4dcac5102e5f8b99b7f7f
Sparta_EN <i@evo.moe>
2026-03-17 15:11:38 +0800

    chore: Update copyright

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="meterpreter-traffic-parser",
-    version="0.0.1",
+    version="0.0.3",
     author="SpartaEN",
     author_email="nya@sparta-en.org",
     description="Meterpreter traffic parser",
